2ad4fc3ea969a9f359881f364b02ba6acdd55b2c
amirhosein-abdolahi <amh.ab.1381@gmail.com>
2025-04-04 23:08:52 +0330

    Part 5

---
+++ part5/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math_utils.h"
+
+int main(int argc, char const *argv[])
+{
+    int x = 5;
+    std::cout << "The square of " << x << " is " << square(x) << std::endl;
+    return 0;
+}
